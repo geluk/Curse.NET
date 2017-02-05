c79f8f9a5a17aa21adb6cf65d0991453a346de27
Johan Geluk <johan@geluk.io>
2017-02-05 04:16:47 +0100

    Add MillisecondEpochConverter

--- Curse.NET/MicrosecondEpochConverter.cs
+++ Curse.NET/MicrosecondEpochConverter.cs
@@ -10,13 +10,29 @@ namespace Curse.NET
 
 		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
 		{
-			writer.WriteRawValue(((DateTime)value - epoch).TotalMilliseconds + "000");
+			writer.WriteRawValue(((int)(((DateTime)value - epoch).TotalMilliseconds * 1000)).ToString());
 		}
 
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
 			if (reader.Value == null) { return null; }
 			return epoch.AddMilliseconds((long)reader.Value / 1000d);
+		}
+	}
+
+	internal class MillisecondEpochConverter : DateTimeConverterBase
+	{
+		private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteRawValue(((int)(((DateTime)value - epoch).TotalMilliseconds)).ToString());
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			if (reader.Value == null) { return null; }
+			return epoch.AddMilliseconds((long)reader.Value);
 		}
 	}
 }
