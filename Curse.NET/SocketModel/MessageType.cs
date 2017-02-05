22b7b521b30532991fbb37fed11d342b6507d646
Johan Geluk <johan@geluk.io>
2017-02-05 04:17:56 +0100

    Add some more message types

--- Curse.NET/SocketModel/MessageType.cs
+++ Curse.NET/SocketModel/MessageType.cs
@@ -7,11 +7,13 @@
 
 	public enum ResponseType
 	{
+		ChannelStatusChanged = 72981382,
 		UserActivityChange = 1260535191,
-		ChannelReference = -695526586,
+		ChannelMarkedRead = -695526586,
 		ChatMessage = -635182161,
 		Login = -815187584,
-		UnknownChange = 149631008
+		MessageChanged = 149631008,
+		Unknown1 = 705131365
 	}
 }
 //
