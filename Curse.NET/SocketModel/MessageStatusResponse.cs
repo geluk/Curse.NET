80e23c3d715f295942889d1d6661354ea81c2ab5
Baggykiin <baggykiin@geluk.io>
2017-04-18 14:54:13 +0200

    Implement MessageStatusResponse model

---
+++ Curse.NET/SocketModel/MessageStatusResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curse.NET.SocketModel
+{
+	public class MessageStatusResponse : ResponseBody
+	{
+		public string ConversationID { get; set; }
+		public MessageStatus Status { get; set; }
+		public string ClientID { get; set; }
+		public string ServerID { get; set; }
+		public object RetryAfter { get; set; }
+		public int ForbiddenReason { get; set; }
+		public int MissingPermission { get; set; }
+	}
+
+	public enum MessageStatus
+	{
+		MessageRefused = 2,
+		Accepted = 4,
+	}
+}
